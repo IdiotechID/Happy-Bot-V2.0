b71a839580e9f46a5a3c1fa140b1cb7227f68023
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-05 21:45:47 +0100

    Now actually updated the time :doh:

--- commands/_time.py
+++ commands/_time.py
@@ -2,10 +2,12 @@ import settings
 from datetime import datetime
 import pytz
 
-timezones = ""
-zone = settings.timezones[0]
-time = datetime.now(pytz.timezone(zone))
-timezones += "**" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
-for zone in settings.timezones[1:]:
+def getTime():
+    timezones = ""
+    zone = settings.timezones[0]
     time = datetime.now(pytz.timezone(zone))
-    timezones += "| **" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
+    timezones += "**" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
+    for zone in settings.timezones[1:]:
+        time = datetime.now(pytz.timezone(zone))
+        timezones += "| **" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
+    return timezones
