9e8b7185f7367da7275f7b1a309afabb6378fcaa
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-05 22:25:59 +0100

    added question handling

--- bot.py
+++ bot.py
@@ -28,15 +28,29 @@ def on_message(message):
     #Only listen to messages not from the bot.
     if message.author.id != client.user.id:
 
+        #I use this a lot and I'm lazy.
+        messagelower = message.content.lower()
+
         #An array of messages to be deleted. Also the only meme on the server.
         deleteThis = []
 
         #checks for trigger words in messages
         for response in autoresponses.responses:
-            if response in message.content:
+            if response in messagelower:
                 yield from client.send_message(message.channel, random.choice(autoresponses.responses[response]))
 
         #checks for trigger words in questions
+        if (message.content[-1] == "?" and "bot" in messagelower) or client.user.mention in message.content:
+            found = False
+            for response in autoresponses.questionResponses:
+                if response in messagelower:
+                    found = True
+                    yield from client.send_message(message.channel, random.choice(autoresponses.questionResponses[response]))
+
+            #If no trigger words yet it is a question for the bot.
+            if not found:
+                #Respond with an 8ball answer.
+                yield from client.send_message(message.channel, random.choice(autoresponses.eightBall))
 
 
         #Checks for a command
@@ -58,7 +72,7 @@ def on_message(message):
             if message.content.startswith(settings.operator):
 
                 if command[0] == "time":
-                    botTalk = yield from client.send_message(message.channel, _time.timezones)
+                    botTalk = yield from client.send_message(message.channel, _time.getTime())
                     deleteThis.append(botTalk)
                     yield from client.delete_message(message)
                     yield from asyncio.sleep(10)
