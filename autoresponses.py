138f88b4fd970381a3fb8c132fdc9876095ba2c3
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-05 22:26:19 +0100

    added question handling

--- autoresponses.py
+++ autoresponses.py
@@ -1,6 +1,6 @@
 #responses for messages contationing the key word
 responses = {
-    "meme" : ["Memes are against the rules!","Please do not meme!","Memes are heresy!"],
+    "meme" or "dank" : ["Memes are against the rules!","Please do not meme!","Memes are heresy!"],
     "night" : ["Sleep Well!","Night!","Good Night!",":sleeping:","Sweet Dreams!"],
     "morning" : ["Hello!","Hi!","Hi Hi!","Welcome!"],
     "bye" or "cya" or "seeya" or "farewell": ["Bye!","Goodbye!","Take Care!","Have Fun!","Farewell!","GLHF!",":punch:"],
@@ -8,9 +8,18 @@ responses = {
     "toaster" : ["I am not a toaster","I do not produce toast","I do not specialise in baked goods","I am not a kitchen appliance","I do not assist with food preparation"],
 }
 
-
 #responses for questions containing the key word
 questionResponses = {
-    "idiotech" : ""
-
+    "idiotech" : ["~~Idiotech~~ SolarPolarMan created me","Idiotech is the Omnissiah","Idiotech is my master","Idiotech is a benevolent ruler","Idiotech requires your money"],
+    "master" : ["Idiotech is my master"],
+    "age" or "old" or "young" or "baby" : ["I have no age","I am infinite"],
+    "food" : ["I like pie!","I like cake!","I like chocolate!","I like ice cream!","I like bacon!","I do not require sustenance!"],
+    "love" or ":heart:" : ["I love everyone!","I love you!","I love cats!","I love everything!","<3"],
+    "sorry" : ["I forgive you","Thank you","Friends?","Ok :)"],
+    "happy" : ["I am always happy!","Being happy is my job!","Always!","Meow!","Woof!"]
 }
+
+#responses for other questions
+eightBall = ["Certainly","Yes!","Definitely","Sure!","Absolutely","Okay","Affirmative",
+"Yeah","Without doubt","Very Well","Maybe","Possibly","Perhaps","Not sure",
+"I'll think about it","No","Not likely","Doubtful","Absolutely not","Negative"]
