4734cc429a89cd4cdb0847969a14464ed9491e71
DevonWC <eskk@mail.com>
2016-09-06 16:17:16 -0300

    Update _time.py

--- commands/_time.py
+++ commands/_time.py
@@ -11,3 +11,24 @@ def getTime():
         time = datetime.now(pytz.timezone(zone))
         timezones += "| **" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
     return timezones
+    
+    def getTimezone(loc):
+    """
+   Function to get local time in cities
+   :return: Dictionary with {"city":"%H:%M"}
+   """
+    places = {'sf' or 'sanfrancisco': 'US/Pacific', 'london' or 'uk' or 'gb': 'Europe/London', 'sydney': 'Australia/Sydney', 'perth': 'Australia/Perth', 'ny' or 'newyork': 'US/Eastern', 'br' or 'brazl': 'Brazil/East', 'manila': 'Asia/Manila'}
+    output = {}
+    now_utc = datetime.now(pytz.timezone('UTC'))
+ 
+ 
+    if loc in places:
+        for i in places:
+            time = now_utc.astimezone(pytz.timezone(places[i]))
+            fmttime = time.strftime('%H:%M')
+            output[i] = fmttime
+
+        return "Current time in **{0}** is **{1}**".format(places[loc], output[loc])
+		
+    else:
+        return "**Location/Timezone not found**"
