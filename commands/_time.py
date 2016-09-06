f6b1cc86c0c7b911d21611a01fea8c4fc679d149
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-06 22:52:35 +0100

    !time now supports custom locations in ALL THE FORMATS
    Standard, ISO Country codes and time zone shorthand are all accepted

--- commands/_time.py
+++ commands/_time.py
@@ -11,24 +11,56 @@ def getTime():
         time = datetime.now(pytz.timezone(zone))
         timezones += "| **" + zone + " " + "**" + ": {0}".format(time.strftime("%I:%M %p")) + " "
     return timezones
-    
+
 def getTimezone(loc):
     """
-   Function to get local time in cities
-   :return: Dictionary with {"city":"%H:%M"}
-   """
-    places = {'sf' or 'sanfrancisco': 'US/Pacific', 'london' or 'uk' or 'gb': 'Europe/London', 'sydney': 'Australia/Sydney', 'perth': 'Australia/Perth', 'ny' or 'newyork': 'US/Eastern', 'br' or 'brazl': 'Brazil/East', 'manila': 'Asia/Manila'}
+    Function to get local time in cities
+    :return: Dictionary with {"city":"%H:%M"}
+    """
+
+    places = {}
     output = {}
     now_utc = datetime.now(pytz.timezone('UTC'))
- 
- 
-    if loc in places:
-        for i in places:
-            time = now_utc.astimezone(pytz.timezone(places[i]))
-            fmttime = time.strftime('%H:%M')
-            output[i] = fmttime
 
-        return "Current time in **{0}** is **{1}**".format(places[loc], output[loc])
-		
+    #Making a dictionary of different possible inputs and their corresponding tz
+    loc = loc.lower()
+
+    #Standard timezone format
+    for zone in pytz.common_timezones:
+        places[zone.lower()] = zone
+
+
+    #Country codes and shorthands
+    for nation in pytz.country_timezones:
+        if type(nation) is list:
+            nation = nation[0]
+
+        longhand = pytz.country_timezones(nation)
+
+        if type(longhand) is list:
+            longhand =longhand[0]
+
+        places[nation.lower()] = longhand
+        try:
+            code = (pytz.timezone(nation).localize(datetime.now()).tzname()).lower()
+            places[code] = longhand
+        except pytz.exceptions.UnknownTimeZoneError:
+            continue
+
+
+    for key, value in places.items():
+
+        if loc in key.split("/"):
+            place = value
+
+        elif loc in key:
+            place = value
+
+
+    if "place" in locals():
+        time = now_utc.astimezone(pytz.timezone(place))
+        fmttime = time.strftime('%I:%M%p')
+        output[loc] = fmttime
+        return "Current time in **{0}** is **{1}**".format(place, output[loc])
     else:
         return "**Location/Timezone not found**"
