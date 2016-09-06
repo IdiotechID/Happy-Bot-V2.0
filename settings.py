750432abfb02e6b1616a956810bb34b39ca42de8
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-06 16:46:59 +0100

    Added support for multiple mod roles. Added !rules. Started giveaway support.

--- settings.py
+++ settings.py
@@ -1,11 +1,14 @@
-modRole = "222359639575101442" #The id of the mod role. Can be found by typing "\@MOD-ROLE-MENTION"
+modRoles = ["222359639575101442"] #The id of the mod role(s). Can be found by typing "\@MOD-ROLE-MENTION"
+
+#What you use to trigger a command (EG: !help OR /help)
 operator = "!"
 
 helpText = """Here are all the current commands:
 <> means an optional argument
 [] means a required argument
 
-`{}time` Prints the time in several timezones""".format(operator)
+`{0}time` Prints the time in several timezones
+""".format(operator)
 
 #timezones to output on !time from TZ row of https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 timezones = [
@@ -13,3 +16,9 @@ timezones = [
 "Europe/Berlin",
 "Australia/Victoria"
 ]
+
+#The id of the giveaway channel(s). Can be found by typing "\#giveaway-channel"
+giveawayChannels = ["222739948153995264"]
+
+#The text displayed for !rules
+rulesText = "Please see <#222739924313440257>"
