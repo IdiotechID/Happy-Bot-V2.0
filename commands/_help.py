994c8eb0b3ba9e6ad93bc56011eaa930e827ad35
SolarPolarMan <Solarpolarman@gmail.com>
2016-09-07 03:31:12 +0100

    !help can now handle fake modules

--- commands/_help.py
+++ commands/_help.py
@@ -2,23 +2,25 @@ import settings
 from commands import _time, joke, youtube, giveaways
 
 #Help vars
-class fakeJokeModule():
+class fakeHelpModule():
     #base call
     call = "help"
 
     #Help text
-    helpText = "It's infinite"
+    helpText = "`!help [Command]`"
 
 moduleLookup = {
     _time.call : _time,
     giveaways.call : giveaways,
     joke.call : joke,
     youtube.call : youtube,
-    fakeJokeModule.call : fakeJokeModule}
+    fakeHelpModule.call : fakeHelpModule}
 
 def getHelp(commandString):
     #takes a string (EG: time) and returns the help for that command
-    return moduleLookup[commandString].helpText
-
+    try:
+        return moduleLookup[commandString].helpText
+    except KeyError:
+        return commandError(fakeHelpModule)
 def commandError(command):
     return "I don't understand what you're saying\nuse `{}help {}` for more info".format(settings.operator, command.call)
